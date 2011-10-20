93249cacd4f6f752c8ba56517c164171aec67da3
cravesoft <cravesoft@gmail.com>
2011-10-20 10:51:01 +0200

    Updated xmonad configuration to work with Unity

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,8 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Config.Azerty
 import qualified Data.Map as M
+import XMonad.Layout.Gaps
+import XMonad.Config.Desktop (desktopLayoutModifiers)
 
 -- Declare config preferences
 config_terminal = "gnome-terminal" -- Default terminal to run
@@ -10,10 +12,21 @@ config_focusFollowsMouse = False
  
 -- Run xmonad with the specified configuration
 main = xmonad myConfig
-  
+
 -- Use the gnomeConfig, but change a couple things
 myConfig = gnomeConfig {
   keys = \c -> azertyKeys c `M.union` keys gnomeConfig c,
   terminal = config_terminal,
+  layoutHook = myLayouts, 
+  manageHook = myManageHook,
   focusFollowsMouse = config_focusFollowsMouse
 }
+
+myManageHook = composeAll (
+    [ manageHook gnomeConfig
+    , className =? "Unity-2d-panel"    --> doIgnore
+    , className =? "Unity-2d-launcher" --> doIgnore
+    ])
+
+myLayouts = gaps [(U, 24)] $
+    desktopLayoutModifiers (Tall 1 0.03 0.5 ||| Full)
