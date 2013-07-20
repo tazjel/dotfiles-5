93430c971c06f1a20c7a4ad29df65a19733fb6d0
cravesoft <cravesoft@gmail.com>
2013-07-20 22:44:15 +0200

    Added script to make a snapshot of /home in external disk

---
+++ bin/make_snapshot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SOURCE_PATH="/home/boule"
+DESTINATION_PATH="/media/WDPassport/home"
+  
+# Check that destination path is valid
+if [ ! -e "$DESTINATION_PATH" ]
+then
+    echo "Error, destination path is not valid"
+    exit
+fi
+        
+rsync -apv --progress --delete --stats --times --perms --filter "- .*/" --filter "- *.tmp" --filter "- *.iso" --filter "- lost+found/" "$SOURCE_PATH" "$DESTINATION_PATH"
+          
+echo "Done."
